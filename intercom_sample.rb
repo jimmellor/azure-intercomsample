aee0e2ed2accd629a9a54fbb80573a530e7d9f9d
Jim Mellor <jim.mellor@me.com>
2016-03-23 22:37:32 +0000

    Log all the things

--- intercom_sample.rb
+++ intercom_sample.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
-
 require 'bundler/setup'
 require 'intercom'
 require 'time'
 require 'azure'
 require 'json'
+require 'logger'
+
+log = Logger.new(STDOUT)
+log.level = Logger::INFO
 
 conf_file = File.read('intercom_sample.conf')
 intercom_sample_settings = JSON.parse(conf_file)
@@ -30,22 +33,33 @@ intercom_data_table = nil
 tables.query_tables.each do |table|
   if table[:properties]['TableName'] == intercom_table_name
     intercom_data_table = table
+    log.info("Azure table #{intercom_table_name} exists: #{intercom_data_table}")
   end
 end
 
 # if the intercom data table doesn't exist, create it.
 if !intercom_data_table
+  log.info("Creating Azure table #{intercom_table_name}")
   tables.create_table(intercom_table_name)
 end
 
+log.debug("Initialising intercom API connection")
 intercom_connection = Intercom::Client.new(app_id: intercom_app_id, api_key: intercom_api_key)
+
+log.info("Beginning update events")
+
+event_count = 0
+event_update_count = 0
 
 intercom_connection.users.all.each do |user|
   # get the time the user last requested something
   user_last_request_at = user.last_request_at.to_i
 
+  event_count += 1
+
   # check it was after the last time we collected data
   if user_last_request_at >= last_request_at
+    event_update_count += 1
     intercom_data_sample = {
     :PartitionKey => intercom_partition_key,
     :RowKey => user_last_request_at.to_s,
@@ -56,6 +70,12 @@ intercom_connection.users.all.each do |user|
     :location_city => user.location_data.city_name,
     :zonza_site => user.custom_attributes["site"]
     }
-    tables.insert_entity(intercom_table_name, intercom_data_sample)
+    log.info("Inserting into #{intercom_table_name} data #{intercom_data_sample}")
+    begin
+      tables.insert_entity(intercom_table_name, intercom_data_sample)
+    rescue Azure::Core::Http::HTTPError => e
+      log.warn("An error occurred updating #{intercom_data_sample}")
+    end
   end
 end
+log.info("Done. #{event_count} event#{"s" if event_count != 1} processed, #{event_update_count} updated.")
